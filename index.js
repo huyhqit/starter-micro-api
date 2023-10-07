8109262b3bf73c3aef84c0dbf25426edf3bb32d3
Ho Quang Huy <37619280+huyhqit@users.noreply.github.com>
2023-10-07 23:31:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { getFinalUrl } = require("./src/utils/helper");
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TOKEN;
-console.log('token')
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
